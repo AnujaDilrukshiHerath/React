77f6e03a7f74293cae4d825c791c420dd9782be8
AnujaDilrukshiHerath <anuanujadilrukshi@gmail.com>
2022-05-24 00:29:04 +0530

    command added

--- react1/src/App.js
+++ react1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello Anuja Samahithan
+         Hello World
         </p>
         <a
           className="App-link"
